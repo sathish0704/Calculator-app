e20079d85baf3ea93e99d64fc8dcd533edf9e509
Sathish Kumar <sathish2267@gmail.com>
2022-07-23 14:52:52 +0530

    Initial commit

---
+++ calculator/src/App.js
@@ -0,0 +1,69 @@
+import React, { useState } from 'react'
+import './App1.css';
+
+
+const App = () => {
+  const [result, setResult] = useState("");
+
+  const handleClick = (e) =>
+  {
+    setResult(result.concat(e.target.name));
+
+  }
+
+  const clear = () =>
+  {
+    setResult("");
+  }
+
+  const backspace = () =>
+  {
+    setResult(result.slice(0,-1));
+    
+  }
+
+  const calculate = () =>
+  {
+    try{
+      setResult(eval(result).toString());
+
+    }
+    catch(err)
+    {
+      setResult("ERROR");
+    }
+    
+  }
+
+  return (
+    <>
+    <div className='conatiner'>
+      <form>
+        <input type="text" value={result} placeholder="SATHISH CALCULATOR"></input>  
+    </form>
+    <div className='keypad'>
+      <button className='higglight' onClick={clear} id="clear" >Clear</button>
+      <button className='higglight' onClick={backspace} id="backspace">C</button>
+      <button name='/' onClick={handleClick}>&divide;</button>
+      <button name='7' onClick={handleClick}>7</button>
+      <button name='8' onClick={handleClick}>8</button>
+      <button name='9' onClick={handleClick}>9</button>
+      <button className='higglight' name='*' onClick={handleClick}>&times;</button>
+      <button name='4' onClick={handleClick}>4</button>
+      <button name='5' onClick={handleClick}>5</button>
+      <button name='6' onClick={handleClick}>6</button>
+      <button className='higglight' name='-' onClick={handleClick}>&ndash;</button>
+      <button name='1' onClick={handleClick}>1</button>
+      <button name='2' onClick={handleClick}>2</button>
+      <button name='3' onClick={handleClick}>3</button>
+      <button className='higglight' name='+' onClick={handleClick}>+</button>
+      <button name='0' onClick={handleClick}>0</button>
+      <button name='.' onClick={handleClick}>.</button>
+      <button className='higglight'  onClick={calculate} id="result">=</button>
+    </div>
+    </div>
+    </>
+  )
+}
+
+export default App
